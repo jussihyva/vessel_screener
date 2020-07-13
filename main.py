2bc8dbd4be58244e58a7ea73b2e0a9f7c7a5c774
Ubuntu <ubuntu@ip-172-31-87-195.ec2.internal>
2020-07-13 19:49:55 +0300

    Vessel screener implementation ongoing. mysql ready.

---
+++ main.py
@@ -0,0 +1,51 @@
+import models
+from fastapi import FastAPI, Request, Depends, BackgroundTasks
+from fastapi.templating import Jinja2Templates
+from database import SessionLocal, engine
+from sqlalchemy.orm import Session
+from pydantic import BaseModel
+from models import Country
+
+app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
+
+templates = Jinja2Templates(directory="templates")
+
+class CountryRequest(BaseModel):
+    symbol: str
+
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+
+@app.get("/")
+def home(request: Request):
+    """
+    Displays country code of vessels in The Aura river at Turku.
+    """
+    return templates.TemplateResponse("home.html",
+    {
+        "request": request,
+        "mmsi": "230xxxxxx"
+    })
+
+@app.post("/stock")
+def create_country(country_request: CountryRequest, db: Session = Depends(get_db)):
+    """
+    Updateas a vellel related information in The Aura river at Turku.
+    """
+
+    country = Country()
+    country.mmsi_mid = country_request.symbol
+
+    db.add(country)
+    db.commit()
+
+    return {
+        "code": "success",
+        "message": "stock created"
+    }
---
+++ models.py
@@ -0,0 +1,21 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    models.py                                          :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: jkauppi <jkauppi@student.hive.fi>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2020/07/13 17:49:07 by jkauppi           #+#    #+#              #
+#    Updated: 2020/07/13 18:55:35 by jkauppi          ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+from sqlalchemy import Column, Integer, String
+from database import Base
+
+class Country(Base):
+	__tablename__ = "country"
+
+	id = Column(Integer, primary_key=True, index=True)
+	mmsi_mid = Column(Integer, unique=True, index=True)
+	country = Column(String)
