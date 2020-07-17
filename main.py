c5c7a93a65419ba734091574fe22faaed387658c
Juhani Kauppi <juhanikauppi@hotmail.com>
2020-07-17 16:45:15 +0300

    country.db removed from repo.

--- main.py
+++ main.py
@@ -56,8 +56,6 @@ def home(request: Request, db: Session = Depends(get_db)):
     timestamp = int(request.query_params["timestamp"])
     country_list = db.query(Country).filter(Country.timestamp > int(timestamp))
     for country in country_list:
-        print(timestamp)
-        print(country.timestamp)
         if country.country == "-":
             country.country = country_name[country.mmsi_mid]
             db.add(country)
