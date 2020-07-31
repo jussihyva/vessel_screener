9c43fb5fda0136710f2903741f538e406a887cc8
Miikka Tuominen <tuommii@gmail.com>
2020-07-31 20:29:08 +0300

    better ui

---
+++ ws-server/static/main.js
@@ -0,0 +1,27 @@
+const app = document.getElementById('app');
+
+window.addEventListener('DOMContentLoaded', () => {
+	const ws = new WebSocket('ws://localhost:8001/ws');
+	ws.onopen = function(event) {
+		console.log('WebSocket connection opened');
+	}
+	ws.onerror = function(error) {
+		console.log(error);
+	}
+	ws.onmessage = function(event) {
+		render(JSON.parse(event.data));
+	}
+});
+
+function render(data) {
+	var list = document.createElement('ul');
+	data.forEach(elem => {
+		const timestamp = elem['Timestamp'];
+		const name = elem['Name'];
+		const item = document.createElement('li');
+		item.innerHTML = name + ' ' + timestamp;
+		list.appendChild(item);
+	});
+	app.innerHTML = '';
+	app.appendChild(list);
+}
