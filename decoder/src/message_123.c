232a39a847d9edaf4711b992c736244f0feb3267
Ubuntu <ubuntu@ip-172-31-87-195.ec2.internal>
2020-08-02 13:36:15 +0300

    log files are zipped. Data structure for AIS messages 1, 2 and 3 completed

--- decoder/src/message_123.c
+++ decoder/src/message_123.c
@@ -3,31 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   message_123.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jkauppi <jkauppi@student.hive.fi>          +#+  +:+       +#+        */
+/*   By: ubuntu <ubuntu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/12 13:19:02 by ubuntu            #+#    #+#             */
-/*   Updated: 2020/07/13 17:55:45 by jkauppi          ###   ########.fr       */
+/*   Updated: 2020/08/02 07:17:49 by ubuntu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "aivdm.h"
 #include "message_123.h"
 
-void	parse_message_123(char *ais_data, t_message *message)
+void	parse_message_123(char *ais_data, t_message_123 *message_123)
 {
 	t_record_123	*record_123;
 
 	record_123 = (t_record_123 *)ft_memalloc(sizeof(*record_123));
 	ft_memcpy(record_123, ais_data, sizeof(*record_123));
-	message->message_id = (int)record_123->message_id;
-	message->mmsi = (size_t)record_123->mmsi3 << 22;
-	message->mmsi += (size_t)record_123->mmsi2 << 14;
-	message->mmsi += (size_t)record_123->mmsi1 << 6;
-	message->mmsi += (size_t)record_123->mmsi0;
-	message->speed_over_ground =
+	message_123->message_id = (int)record_123->message_id;
+	message_123->mmsi = (size_t)record_123->mmsi3 << 22;
+	message_123->mmsi += (size_t)record_123->mmsi2 << 14;
+	message_123->mmsi += (size_t)record_123->mmsi1 << 6;
+	message_123->mmsi += (size_t)record_123->mmsi0;
+	message_123->speed_over_ground =
 						(double)((size_t)record_123->speed_over_ground_1 << 4);
-	message->speed_over_ground += (double)record_123->speed_over_ground_0;
-	message->speed_over_ground /= 10;
+	message_123->speed_over_ground += (double)record_123->speed_over_ground_0;
+	message_123->speed_over_ground /= 10;
 	free(record_123);
 	return ;
 }
--- decoder/src/message_123.h
+++ decoder/src/message_123.h
@@ -3,46 +3,56 @@
 /*                                                        :::      ::::::::   */
 /*   message_123.h                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jkauppi <jkauppi@student.hive.fi>          +#+  +:+       +#+        */
+/*   By: ubuntu <ubuntu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/12 14:06:28 by ubuntu            #+#    #+#             */
-/*   Updated: 2020/07/13 16:53:32 by jkauppi          ###   ########.fr       */
+/*   Updated: 2020/08/02 06:52:54 by ubuntu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/* Recommendation ITU-R M.1371-5 */
 
 #ifndef MESSAGE_123_H
 # define MESSAGE_123_H
 
 typedef struct		s_record_123
 {
-	unsigned char		dummy_1					:	2,
-						message_id				:	6;
+	unsigned char		repeat_indicator				:	2,
+						message_id						:	6;
 	unsigned char		mmsi3;
 	unsigned char		mmsi2;
 	unsigned char		mmsi1;
-	unsigned char		navigational_status_1	:	2,
-						mmsi0					: 	6;
-	unsigned char		rate_of_turn_1			:	6,
-						navigational_status_0	:	2;
-	unsigned char		speed_over_ground_1		:	6,
-						rate_of_turn_0			:	2;
-	unsigned char		longitude_4				:	3,
-						position_accuracy		:	1,
-						speed_over_ground_0		:	4;
-	unsigned char		longitude_3				:	8;
-	unsigned char		longitude_2				:	8;
-	unsigned char		longitude_1				:	8;
-	unsigned char		latitude_3				:	7,
-						longitude_0				:	1;
-	unsigned char		latitude_2				:	8;
-	unsigned char		latitude_1				:	8;
-	unsigned char		course_over_ground_2	:	3,
-						latitude_0				:	5;
-	unsigned char		course_over_ground_1	:	8;
-	unsigned char		true_heading_1			:	7,
-						course_over_ground_0	:	1;
-	unsigned char		dummy_2					:	6,
-						true_heading_0			:	2;
+	unsigned char		navigational_status_1			:	2,
+						mmsi0							: 	6;
+	unsigned char		rate_of_turn_1					:	6,
+						navigational_status_0			:	2;
+	unsigned char		speed_over_ground_1				:	6,
+						rate_of_turn_0					:	2;
+	unsigned char		longitude_4						:	3,
+						position_accuracy				:	1,
+						speed_over_ground_0				:	4;
+	unsigned char		longitude_3						:	8;
+	unsigned char		longitude_2						:	8;
+	unsigned char		longitude_1						:	8;
+	unsigned char		latitude_3						:	7,
+						longitude_0						:	1;
+	unsigned char		latitude_2						:	8;
+	unsigned char		latitude_1						:	8;
+	unsigned char		course_over_ground_2			:	3,
+						latitude_0						:	5;
+	unsigned char		course_over_ground_1			:	8;
+	unsigned char		true_heading_1					:	7,
+						course_over_ground_0			:	1;
+	unsigned char		timestamp						:	6,
+						true_heading_0					:	2;
+	unsigned char		communication_state_3			:	2,
+						raim_flag						:	1,
+						spare							:	3,
+						special_manoeuvre_indicator		:	2;
+	unsigned char		communication_state_2			:	8;
+	unsigned char		communication_state_1			:	8;
+	unsigned char		dummy_1							:	7,
+						communication_state_0			:	1;
 }					t_record_123;
 
 #endif
