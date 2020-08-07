d06cf86467beb32107f6db2141936b80f1ea4a6b
Ubuntu <ubuntu@ip-172-31-57-241.ec2.internal>
2020-08-07 15:12:39 +0300

    New SQL tables message_1 and _3. New tab for the web page.

--- decoder/src/payload_parser.c
+++ decoder/src/payload_parser.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   payload_parser.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ubuntu <ubuntu@student.42.fr>              +#+  +:+       +#+        */
+/*   By: jkauppi <jkauppi@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/18 11:46:05 by ubuntu            #+#    #+#             */
-/*   Updated: 2020/08/02 22:51:15 by ubuntu           ###   ########.fr       */
+/*   Updated: 2020/08/06 13:36:09 by jkauppi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,29 +22,32 @@ static void		remove_cr_char(char *line)
 	return ;
 }
 
-static char		*search_record_start_point(char *line, int *ais_dispatcher_timestamp)
+static char		*search_record_start_point(char *line, t_message_123 *message_123)
 {
-	char	*start_ptr;
-	char	*timestamp_str;
-	char	saved_char;
+	char			*start_ptr;
+	char			*timestamp_str;
+	char			saved_char;
+	struct timeval	tv;
 
+	gettimeofday(&tv, NULL);
 	start_ptr = ft_strstr(line, "!AIVDM");
 	saved_char = *start_ptr;
 	*start_ptr = '\0';
 	timestamp_str = ft_strstr(line, "c:");
 	*start_ptr = saved_char;
+	message_123->timestamp.decoder = tv.tv_sec;
 	if (timestamp_str)
-		*ais_dispatcher_timestamp = ft_atoi(timestamp_str + 2);
+		message_123->timestamp.ais_dispatcher = ft_atoi(timestamp_str + 2);
 	return (start_ptr);
 }
 
-char			**parse_input_line(char *line, int *ais_dispatcher_timestamp)
+char			**parse_input_line(char *line, t_message_123 *message_123)
 {
 	char	*start_ptr;
 	char	**aivdm_record;
 
 	remove_cr_char(line);
-	start_ptr = search_record_start_point(line, ais_dispatcher_timestamp);
+	start_ptr = search_record_start_point(line, message_123);
 	aivdm_record = (char **)ft_strsplit(start_ptr, ',');
 	return (aivdm_record);
 }
