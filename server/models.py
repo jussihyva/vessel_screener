6239374722266a2e9cd1709f968c9cef9d2d71d4
Ubuntu <ubuntu@ip-172-31-57-241.ec2.internal>
2020-08-07 15:12:39 +0300

    New SQL tables message_1 and _3. New tab for the web page.

--- server/models.py
+++ server/models.py
@@ -3,14 +3,15 @@
 #                                                         :::      ::::::::    #
 #    models.py                                          :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: ubuntu <ubuntu@student.42.fr>              +#+  +:+       +#+         #
+#    By: jkauppi <jkauppi@student.hive.fi>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/07/13 17:49:07 by jkauppi           #+#    #+#              #
-#    Updated: 2020/07/15 07:43:39 by ubuntu           ###   ########.fr        #
+#    Updated: 2020/08/06 13:42:10 by jkauppi          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime, Float
+from sqlalchemy.sql import func
 from database import Base
 
 class Country(Base):
@@ -21,3 +22,75 @@ class Country(Base):
 	country = Column(String)
 	timestamp = Column(Integer)
 	comment = Column(String)
+
+class Message_1(Base):
+	__tablename__ = "message_1"
+
+	id = Column(Integer, primary_key=True, index=True)
+	decoder_time = Column(Integer, unique=False, index=True)
+	ais_dispatcher_time = Column(Integer, unique=False, index=True)
+	message_id = Column(Integer)
+	repeat_indicator = Column(Integer)
+	mmsi = Column(Integer)
+	navigational_status = Column(Integer)
+	rate_of_turn = Column(Integer)
+	speed_over_ground = Column(Float)
+	position_accuracy = Column(Integer)
+	longitude = Column(Integer)
+	latitude = Column(Integer)
+	course_over_ground = Column(Integer)
+	true_heading = Column(Integer)
+	timestamp = Column(Integer)
+	special_manoeuvre_indicator = Column(Integer)
+	spare = Column(Integer)
+	raim_flag = Column(Integer)
+	communication_state = Column(Integer)
+	dummy = Column(Integer)
+
+class Message_2(Base):
+	__tablename__ = "message_2"
+
+	id = Column(Integer, primary_key=True, index=True)
+	decoder_time = Column(Integer, unique=False, index=True)
+	ais_dispatcher_time = Column(Integer, unique=False, index=True)
+	message_id = Column(Integer)
+	repeat_indicator = Column(Integer)
+	mmsi = Column(Integer)
+	navigational_status = Column(Integer)
+	rate_of_turn = Column(Integer)
+	speed_over_ground = Column(Float)
+	position_accuracy = Column(Integer)
+	longitude = Column(Integer)
+	latitude = Column(Integer)
+	course_over_ground = Column(Integer)
+	true_heading = Column(Integer)
+	timestamp = Column(Integer)
+	special_manoeuvre_indicator = Column(Integer)
+	spare = Column(Integer)
+	raim_flag = Column(Integer)
+	communication_state = Column(Integer)
+	dummy = Column(Integer)
+
+class Message_3(Base):
+	__tablename__ = "message_3"
+
+	id = Column(Integer, primary_key=True, index=True)
+	decoder_time = Column(Integer, unique=False, index=True)
+	ais_dispatcher_time = Column(Integer, unique=False, index=True)
+	message_id = Column(Integer)
+	repeat_indicator = Column(Integer)
+	mmsi = Column(Integer)
+	navigational_status = Column(Integer)
+	rate_of_turn = Column(Integer)
+	speed_over_ground = Column(Float)
+	position_accuracy = Column(Integer)
+	longitude = Column(Integer)
+	latitude = Column(Integer)
+	course_over_ground = Column(Integer)
+	true_heading = Column(Integer)
+	timestamp = Column(Integer)
+	special_manoeuvre_indicator = Column(Integer)
+	spare = Column(Integer)
+	raim_flag = Column(Integer)
+	communication_state = Column(Integer)
+	dummy = Column(Integer)
--- server/static/js/main.js
+++ server/static/js/main.js
@@ -1,17 +1,23 @@
 
 const useSocket = false;
 
+let	myInterval = null;
 let timeFilterSec = 15 * 60;
 const refreshInterval = 3000;
 const timeFilter = document.getElementById("time_period");
+const selectedTabCountry = document.getElementById("selected_tab_country");
+const selectedTabMmsi = document.getElementById("selected_tab_mmsi");
 const countryTable = document.getElementById("country_table");
 
 window.addEventListener('DOMContentLoaded', () => {
 
 	if (!useSocket)
 	{
-		setInterval(function () { updatePage(); }, refreshInterval);
+		timeFilterChanged();
+		myInterval = setInterval(updatePage_Country, refreshInterval);
 		timeFilter.onchange = timeFilterChanged;
+		selectedTabCountry.onclick = openCountries;
+		selectedTabMmsi.onclick = openMMSI_List;
 		return ;
 	}
 
@@ -25,22 +31,56 @@ window.addEventListener('DOMContentLoaded', () => {
 	ws.onmessage = function(event) {
 		console.log(event.data);
 	}
-
 });
 
 function timeFilterChanged() {
 	timeFilterSec = timeFilter.value;
 	console.log('value changed', timeFilterSec);
-	updatePage();
+	updatePage_Country();
 }
 
-function updatePage() {
+function updatePage_Country() {
 	const timestamp = parseInt((Date.now() / 1000) - timeFilterSec, 10);
-	fetch('/table?timestamp='+ timestamp)
+	fetch('/table_country?timestamp='+ timestamp)
 	.then((res) => {
 		return res.text()
 	})
 	.then((text) => {
 		countryTable.innerHTML = text;
 	})
+}
+
+function updatePage_MMSI() {
+	const timestamp = parseInt((Date.now() / 1000) - timeFilterSec, 10);
+	fetch('/table_mmsi?timestamp='+ timestamp)
+	.then((res) => {
+		return res.text()
+	})
+	.then((text) => {
+		countryTable.innerHTML = text;
+	})
+}
+
+function openCountries()
+{
+	console.log("openCountries");
+	countryTable.innerHTML = "";
+	updatePage_Country();
+	if (myInterval != null)
+	{
+		clearInterval(myInterval);
+		myInterval = null;
+	}
+	myInterval = setInterval(updatePage_Country, refreshInterval);
+}
+function openMMSI_List()
+{
+	if (myInterval != null)
+	{
+		clearInterval(myInterval);
+		myInterval = null;
+	}
+	myInterval = setInterval(updatePage_MMSI, refreshInterval);
+	console.log("openMMSI_List");
+	countryTable.innerHTML = "POO";
 }
