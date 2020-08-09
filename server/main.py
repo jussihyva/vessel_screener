3b65be5b1053298ede1b9a1bcba1c9c6df2d4345
Juhani Kauppi <juhanikauppi@hotmail.com>
2020-08-09 15:17:57 +0300

    Unused functions removed from main.py

--- server/main.py
+++ server/main.py
@@ -1,13 +1,12 @@
 import models
-import yfinance
-from country import country_name
 from fastapi import FastAPI, Request, Depends, BackgroundTasks
 from fastapi.templating import Jinja2Templates
 from database import SessionLocal, engine
 from sqlalchemy.orm import Session
 from pydantic import BaseModel
-from models import Country
 from fastapi.staticfiles import StaticFiles
+from main_mmsi import *
+from main_country import *
 
 app = FastAPI()
 
@@ -63,51 +62,9 @@ def table_mmsi(request: Request, db: Session = Depends(get_db)):
     Displays country code of vessels in The Aura river at Turku.
     """
 
-    country_list = get_country_list(request, db)
+    country_list = get_mmsi_list(request, db)
     return templates.TemplateResponse("table_mmsi.html",
     {
         "request": request,
         "country": country_list
     })
-
-def fetch_country_data(mmsi_mid: int):
-    db = SessionLocal()
-    country = db.query(Country).filter(Country.mmsi_mid == mmsi_mid).first()
-    country.country = country_name[mmsi_mid]
-    db.add(country)
-    db.commit()
-
-@app.post("/country")
-async def create_country(country_request: CountryRequest, backround_task: BackgroundTasks, db: Session = Depends(get_db)):
-    """
-    Updateas a vellel related information in The Aura river at Turku.
-    """
-    country = db.query(Country).filter(Country.mmsi_mid == country_request.symbol).first()
-    if (country == None):
-        country = Country()
-        country.mmsi_mid = country_request.symbol
-        country.timestamp = country_request.timestamp
-        country.comment = "Only for test. This is NOT correct information."
-        db.add(country)
-        db.commit()
-        backround_task.add_task(fetch_country_data, country.mmsi_mid)
-    else:
-        print(country_request.timestamp)
-        country.timestamp = country_request.timestamp
-        db.add(country)
-        db.commit()
-
-    return {
-        "code": "success",
-        "message": "country created"
-    }
-
-def get_country_list(request, db):
-    timestamp = int(request.query_params["timestamp"])
-    country_list = db.query(Country).filter(Country.timestamp > int(timestamp))
-    for country in country_list:
-        if country.country == "-":
-            country.country = country_name[country.mmsi_mid]
-            db.add(country)
-            db.commit()
-    return(country_list)
