665b6d7e99b7d1e4666764dfa4b33c4f858ed424
Juhani Kauppi <juhanikauppi@hotmail.com>
2020-08-09 15:17:57 +0300

    Unused functions removed from main.py

---
+++ server/main_country.py
@@ -0,0 +1,12 @@
+from country import country_name
+from models import Country
+
+def get_country_list(request, db):
+    timestamp = int(request.query_params["timestamp"])
+    country_list = db.query(Country).filter(Country.timestamp > int(timestamp))
+    for country in country_list:
+        if country.country == "-":
+            country.country = country_name[country.mmsi_mid]
+            db.add(country)
+            db.commit()
+    return(country_list)
