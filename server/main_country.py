dd7f9f5e5a1a472cb599b4cf9e999a0d3e157e13
Ubuntu <ubuntu@ip-172-31-57-241.ec2.internal>
2020-08-10 10:40:24 +0300

    Create content of response moved from main.py to main_mmsi.py and main_country.py

--- server/main_country.py
+++ server/main_country.py
@@ -1,5 +1,8 @@
+from fastapi.templating import Jinja2Templates
 from country import country_name
 from models import Country
+
+templates = Jinja2Templates(directory="templates")
 
 def get_country_list(request, db):
     timestamp = int(request.query_params["timestamp"])
@@ -9,4 +12,8 @@ def get_country_list(request, db):
             country.country = country_name[country.mmsi_mid]
             db.add(country)
             db.commit()
-    return(country_list)
+    return templates.TemplateResponse("table_country.html",
+    {
+        "request": request,
+        "country": country_list
+    })
