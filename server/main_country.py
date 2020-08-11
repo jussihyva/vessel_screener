bd428b40968b105472885e50398633a24dc6e738
Ubuntu <ubuntu@ip-172-31-57-241.ec2.internal>
2020-08-11 07:51:11 +0300

    Malta (229) added to country list.

--- server/main_country.py
+++ server/main_country.py
@@ -8,7 +8,7 @@ def get_country_list(request, db):
     timestamp = int(request.query_params["timestamp"])
     country_list = db.query(Country).filter(Country.timestamp > int(timestamp))
     for country in country_list:
-        if country.country == "-":
+        if country.country in ["-", None]:
             country.country = country_name[country.mmsi_mid]
             db.add(country)
             db.commit()
