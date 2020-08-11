2429cbfb262501cd4d3392e37dcb04e8c9fa56f8
Juhani Kauppi <juhanikauppi@hotmail.com>
2020-08-11 14:59:03 +0300

    Number of vessel column implemented.

--- server/main_mmsi.py
+++ server/main_mmsi.py
@@ -1,15 +1,15 @@
 from fastapi.templating import Jinja2Templates
-from country import country_name
+from country import country_name_list
 from models import Country
 
 templates = Jinja2Templates(directory="templates")
 
-def get_mmsi_list(request, db):
+def get_mmsi_table(request, db):
     timestamp = int(request.query_params["timestamp"])
     country_list = db.query(Country).filter(Country.timestamp > int(timestamp))
     for country in country_list:
         if country.country == "-":
-            country.country = country_name[country.mmsi_mid]
+            country.country = country_name_list[country.mmsi_mid]
             db.add(country)
             db.commit()
     return templates.TemplateResponse("table_mmsi.html",
