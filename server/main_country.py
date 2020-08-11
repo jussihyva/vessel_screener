039428f931ec9f348081c6e7ee4b66586cfae9f9
Juhani Kauppi <juhanikauppi@hotmail.com>
2020-08-11 14:59:03 +0300

    Number of vessel column implemented.

--- server/main_country.py
+++ server/main_country.py
@@ -1,33 +1,37 @@
 from fastapi.templating import Jinja2Templates
-from country import country_name
-from models import Country, Message_3
+from country import country_name_list
+from models import Country, Message_1, Message_2, Message_3
 from pydantic import BaseModel
-from sqlalchemy import func
+from sqlalchemy import func, distinct
 
 templates = Jinja2Templates(directory="templates")
 
 class Country_data(BaseModel):
     mid: int
     num_of_mmsi: int
-    country: str
+    country_name: str
 
-def get_country_list(request, db):
+def get_country_table(request, db):
     country_data_list : Country_data = []
     timestamp = int(request.query_params["timestamp"])
-    country_list = db.query(Country).filter(Country.timestamp > int(timestamp))
-    message_3_list = db.query(Message_3.mid, func.count(Message_3.mid)).filter(Message_3.ais_dispatcher_time > int(timestamp)).group_by(Message_3.mid)
-    print(message_3_list)
+    num_of_message_1 = db.query(func.count(Message_1.mmsi)).filter(Message_1.ais_dispatcher_time > \
+        int(timestamp))[0][0]
+    num_of_message_2 = db.query(func.count(Message_2.mmsi)).filter(Message_2.ais_dispatcher_time > \
+        int(timestamp))[0][0]
+    num_of_message_3 = db.query(func.count(Message_3.mmsi)).filter(Message_3.ais_dispatcher_time > \
+        int(timestamp))[0][0]
+    print(num_of_message_1)
+    print(num_of_message_2)
+    print(num_of_message_3)
+    message_3_list = db.query(Message_3.mid, \
+        func.count(distinct(Message_3.mmsi))).filter(Message_3.ais_dispatcher_time > \
+        int(timestamp)).group_by(Message_3.mid)
     for message in message_3_list:
-        message_data = Country_data(mid = message.mid, num_of_mmsi = message[1], country = country_name[message.mid])
-        country_data_list.append(message_data)
-        print(message_data)
-    for country in country_list:
-        if country.country in ["-", None]:
-            country.country = country_name[country.mmsi_mid]
-            db.add(country)
-            db.commit()
+        country_data = Country_data(mid = message.mid, num_of_mmsi = message[1], \
+                        country_name = country_name_list[message.mid])
+        country_data_list.append(country_data)
     return templates.TemplateResponse("table_country.html",
     {
         "request": request,
-        "country": country_data_list
+        "country_data_list": country_data_list
     })
